1364644c177cc3c060f0102b7f5b900e6e69bd71
Kevin Do <kevind0501@gmail.com>
2022-04-01 11:11:53 -0700

    test for getting posts

--- __tests__/posts.test.js
+++ __tests__/posts.test.js
@@ -3,6 +3,8 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+jest.mock('../lib/utils/github');
+
 describe('. routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -12,19 +14,43 @@ describe('. routes', () => {
     pool.end();
   });
 
-  it('creates a post if user is currently signed in', async () => {
+  it('user can get posts', async () => {
     const agent = request.agent(app);
-    const loginReq = await request
+    await agent.get('/api/v1/github/login');
+
+    await request
       .agent(app)
-      .get(`/api/v1/github/login/callback?code=42`)
+      .get('/api/v1/github/login/callback?code=42')
       .redirects(1);
 
-    const res = await request(app).post('/api/v1/posts').send({
-      text: 'this is the first post',
-    });
-    expect(res.body).toEqual({
+    const post1 = {
       id: expect.any(String),
       text: 'this is the first post',
-    });
+    };
+
+    const post2 = {
+      id: expect.any(String),
+      text: 'this is the first post',
+    };
+
+    const res = await agent.get('/api/v1/posts/');
+
+    expect(res.body).toEqual([post1, post2]);
   });
+
+  // it('creates a post if user is currently signed in', async () => {
+  //   const agent = request.agent(app);
+  //   const loginReq = await request
+  //     .agent(app)
+  //     .get(`/api/v1/github/login/callback?code=42`)
+  //     .redirects(1);
+
+  //   const res = await request(app).post('/api/v1/posts').send({
+  //     text: 'this is the first post',
+  //   });
+  //   expect(res.body).toEqual({
+  //     id: expect.any(String),
+  //     text: 'this is the first post',
+  //   });
+  // });
 });
