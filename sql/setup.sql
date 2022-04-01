54fbf7fc6ed44357e5dc4d8000c1a2011f6dc030
Kevin Do <kevind0501@gmail.com>
2022-04-01 11:38:20 -0700

    seeded data

--- sql/setup.sql
+++ sql/setup.sql
@@ -15,3 +15,9 @@ CREATE TABLE posts (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   text VARCHAR(255) NOT NULL
 );
+
+INSERT INTO
+  posts(text)
+VALUES
+  ('this is the first post'),
+  ('this is the second post');
